a51f9dc78d5b067753d5ac055f37056da56b8a95
martynas <martynaskvietelaitis@icloud.com>
2025-03-05 20:02:49 +0200

    grazus zodziai

--- froggydoro/lib/main.dart
+++ froggydoro/lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  //zajibal blet kodel neveikia gitas
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
