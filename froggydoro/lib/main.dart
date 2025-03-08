a782ce1447fb9c98d5ccac849dca602d9187ca39
martynas <martynaskvietelaitis@icloud.com>
2025-03-08 16:52:57 +0200

    Theme Bug Fix
    Fixes a bug that causes the app to revert back to light mode after restarting it.

--- froggydoro/lib/main.dart
+++ froggydoro/lib/main.dart
@@ -1,10 +1,6 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:froggydoro/screens/main_screen.dart';
-import 'package:froggydoro/screens/timer_screen.dart';
-import 'package:froggydoro/screens/settings_screen.dart';
-import 'package:froggydoro/widgets/time_picker.dart';
-import 'package:numberpicker/numberpicker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,6 +15,49 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ThemeMode _themeMode = ThemeMode.system;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadThemeMode();
+  }
+
+  Future<void> _loadThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeMode = prefs.getString('themeMode') ?? 'system';
+    setState(() {
+      _themeMode = _getThemeModeFromString(themeMode);
+    });
+  }
+
+  Future<void> _saveThemeMode(ThemeMode themeMode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('themeMode', _getStringFromThemeMode(themeMode));
+  }
+
+  ThemeMode _getThemeModeFromString(String themeMode) {
+    switch (themeMode) {
+      case 'light':
+        return ThemeMode.light;
+      case 'dark':
+        return ThemeMode.dark;
+      case 'system':
+      default:
+        return ThemeMode.system;
+    }
+  }
+
+  String _getStringFromThemeMode(ThemeMode themeMode) {
+    switch (themeMode) {
+      case ThemeMode.light:
+        return 'light';
+      case ThemeMode.dark:
+        return 'dark';
+      case ThemeMode.system:
+      default:
+        return 'system';
+    }
+  }
 
   void _onThemeModeChanged(ThemeMode mode) {
     setState(() {
