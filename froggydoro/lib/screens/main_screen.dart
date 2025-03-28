515c465e78a693bd436ac9bb0f706663041bdc92
Kiranamus <kastautas8@gmail.com>
2025-03-28 15:05:41 +0200

    Tests for time settings

--- froggydoro/lib/screens/main_screen.dart
+++ froggydoro/lib/screens/main_screen.dart
@@ -11,10 +11,18 @@ class MainScreen extends StatefulWidget {
   const MainScreen({super.key, required this.onThemeModeChanged});
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  State<MainScreen> createState() => MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen>
+class Counter {
+  int value = 0;
+
+  void increment() => value++;
+
+  void decrement() => value--;
+}
+
+class MainScreenState extends State<MainScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
   int _selectedIndex = 0;
@@ -26,8 +34,10 @@ class _MainScreenState extends State<MainScreen>
   int _totalSeconds = 0;
   bool _isBreakTime = false;
   Timer? _timer;
-  bool _isRunning = false;
+  bool isRunning = false;
   int _sessionCount = 0;
+
+  int getSeconds() => _totalSeconds;
 
   @override
   void initState() {
@@ -77,13 +87,17 @@ class _MainScreenState extends State<MainScreen>
     _resetTimer();
   }
 
+  void updateTimer(workMinutes, workSeconds, breakMinutes, breakSeconds) {
+    _updateTimer(workMinutes, workSeconds, breakMinutes, breakSeconds);
+  }
+
   void _startTimer() {
-    if (_isRunning || (_workMinutes == 0 && _workSeconds == 0)) return;
+    if (isRunning || (_workMinutes == 0 && _workSeconds == 0)) return;
 
     AudioManager().playMusic();
 
     setState(() {
-      _isRunning = true;
+      isRunning = true;
     });
 
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
@@ -148,7 +162,7 @@ class _MainScreenState extends State<MainScreen>
     AudioManager().pauseMusic();
     _timer?.cancel();
     setState(() {
-      _isRunning = false;
+      isRunning = false;
     });
   }
 
@@ -188,6 +202,11 @@ class _MainScreenState extends State<MainScreen>
       _breakSeconds = prefs.getInt('breakSeconds') ?? 0;
       _updateTimer(_workMinutes, _workSeconds, _breakMinutes, _breakSeconds);
     });
+  }
+
+  // For test
+  void loadTime() {
+    _loadTimeSettings();
   }
 
   void _showSessionCompletePopup(
@@ -244,8 +263,8 @@ class _MainScreenState extends State<MainScreen>
                 const SizedBox(height: 16.0),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: buttonColor, // Button background color
-                    foregroundColor: textColor, // Button text color
+                    backgroundColor: buttonColor, 
+                    foregroundColor: textColor, 
                   ),
                   onPressed: () {
                     Navigator.pop(context);
--- froggydoro/test/widget_test.dart
+++ froggydoro/test/widget_test.dart
@@ -1,30 +1,151 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:froggydoro/screens/time_settings_screen.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:froggydoro/main.dart';
+class MockUpdateTimer extends Mock {
+  void call(int workMinutes, int workSeconds, int breakMinutes, int breakSeconds);
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('TimeSettingsScreen', () {
+    late MockUpdateTimer mockUpdateTimer;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    setUp(() {
+      mockUpdateTimer = MockUpdateTimer();
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    testWidgets('Load and displays time from shared preferences', (tester) async {
+      // Simulate SharedPreferences
+      SharedPreferences.setMockInitialValues({
+        'workMinutes': 25,
+        'breakMinutes': 5,
+      });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Build the widget tree
+      await tester.pumpWidget(
+        MaterialApp(
+          home: TimeSettingsScreen(updateTimer: mockUpdateTimer),
+        ),
+      );
+
+      // Verify the initial work and break time values
+      expect(find.text('Work Time'), findsOneWidget);
+      expect(find.text('25 min'), findsOneWidget);
+      expect(find.text('Break Time'), findsOneWidget);
+      expect(find.text('5 min'), findsOneWidget);
+    });
+
+    testWidgets('Add work time', (tester) async {
+      // Simulate SharedPreferences
+      SharedPreferences.setMockInitialValues({
+        'workMinutes': 25,
+        'breakMinutes': 5,
+      });
+
+      // Build the widget tree
+      await tester.pumpWidget(
+        MaterialApp(
+          home: TimeSettingsScreen(updateTimer: mockUpdateTimer),
+        ),
+      );
+
+      // Tap on the increment button for work time
+      await tester.tap(find.byIcon(Icons.chevron_right).first);
+      await tester.pump();
+
+      // Verify that the work time has increased by 5 minutes
+      expect(find.text('30 min'), findsOneWidget);
+    });
+
+    testWidgets('Subtracts work time', (tester) async {
+      // Simulate SharedPreferences
+      SharedPreferences.setMockInitialValues({
+        'workMinutes': 25,
+        'breakMinutes': 5,
+      });
+
+      // Build the widget tree
+      await tester.pumpWidget(
+        MaterialApp(
+          home: TimeSettingsScreen(updateTimer: mockUpdateTimer),
+        ),
+      );
+
+      // Tap on the decrement button for work time
+      await tester.tap(find.byIcon(Icons.chevron_left).first);
+      await tester.pump();
+
+      // Verify that the work time has decreased by 5 minutes
+      expect(find.text('20 min'), findsOneWidget);
+    });
+
+    testWidgets('Set timer', (tester) async {
+      // Simulate SharedPreferences
+      SharedPreferences.setMockInitialValues({
+        'workMinutes': 25,
+        'breakMinutes': 5,
+      });
+
+      // Build the widget tree
+      await tester.pumpWidget(
+        MaterialApp(
+          home: TimeSettingsScreen(updateTimer: mockUpdateTimer),
+        ),
+      );
+
+      // Tap on the "Set" button
+      await tester.tap(find.text('Set'));
+      await tester.pump();
+
+      // Verify that the updateTimer function was called with correct values
+      verify(mockUpdateTimer(25, 0, 5, 0)).called(1);
+    });
+
+    testWidgets('Add break time', (tester) async {
+      // Simulate SharedPreferences
+      SharedPreferences.setMockInitialValues({
+        'workMinutes': 25,
+        'breakMinutes': 10,
+      });
+
+      // Build the widget tree
+      await tester.pumpWidget(
+        MaterialApp(
+          home: TimeSettingsScreen(updateTimer: mockUpdateTimer),
+        ),
+      );
+
+      // Tap on the increment button for work time
+      await tester.tap(find.byIcon(Icons.chevron_right).last);
+      await tester.pump();
+
+      // Verify that the work time has increased by 5 minutes
+      expect(find.text('15 min'), findsOneWidget);
+    });
+
+    testWidgets('Subtracts break time', (tester) async {
+      // Simulate SharedPreferences
+      SharedPreferences.setMockInitialValues({
+        'workMinutes': 25,
+        'breakMinutes': 10,
+      });
+
+      // Build the widget tree
+      await tester.pumpWidget(
+        MaterialApp(
+          home: TimeSettingsScreen(updateTimer: mockUpdateTimer),
+        ),
+      );
+
+      // Tap on the decrement button for work time
+      await tester.tap(find.byIcon(Icons.chevron_left).last);
+      await tester.pump();
+
+      // Verify that the work time has decreased by 5 minutes
+      expect(find.text('5 min'), findsOneWidget);
+    });
   });
 }
+
