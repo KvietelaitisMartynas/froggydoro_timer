f25a92fa6c954e8ae0fb32439202b5adbc238d81
Kiranamus <kastautas8@gmail.com>
2025-04-28 16:04:38 +0300

    aa

--- froggydoro/custom_rules/lib/custom_rules.dart
+++ froggydoro/custom_rules/lib/custom_rules.dart
@@ -2,14 +2,12 @@ library custom_rules;
 
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'src/import_sorting_rule.dart'; // Import your rule
-import 'src/rule34.dart';
 
 PluginBase createPlugin() => _CustomRulesPlugin();
 
 class _CustomRulesPlugin extends PluginBase {
   @override
   List<LintRule> getLintRules(CustomLintConfigs configs) => [
-        NoTodoCommentsRule(),
         ImportSortingRule(), // Add your rule here
       ];
 }
--- froggydoro/custom_rules/lib/src/import_sorting_rule.dart
+++ froggydoro/custom_rules/lib/src/import_sorting_rule.dart
@@ -1,22 +1,23 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart' as custom_lint;
-import 'package:analyzer/error/listener.dart';
 import 'package:analyzer/error/error.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/error/listener.dart';
 
-custom_lint.PluginBase createPlugin() => _NoTodoCommentsLinter();
+custom_lint.PluginBase createPlugin() => _ImportSortingLinter();
 
-class _NoTodoCommentsLinter extends custom_lint.PluginBase {
+class _ImportSortingLinter extends custom_lint.PluginBase {
   @override
   List<custom_lint.LintRule> getLintRules(custom_lint.CustomLintConfigs configs) => [
-        NoTodoCommentsRule(),
+        ImportSortingRule(),
       ];
 }
 
-class NoTodoCommentsRule extends custom_lint.DartLintRule {
-  NoTodoCommentsRule() : super(code: _code);
+class ImportSortingRule extends custom_lint.DartLintRule {
+  ImportSortingRule() : super(code: _code);
 
   static const _code = custom_lint.LintCode(
-    name: 'no_todo_comments',
-    problemMessage: 'TODO comments are not allowed.',
+    name: 'import_sorting_rule',
+    problemMessage: 'Imports must be sorted alphabetically. wawawiwa',
     errorSeverity: ErrorSeverity.WARNING,
   );
 
@@ -26,9 +27,19 @@ class NoTodoCommentsRule extends custom_lint.DartLintRule {
     ErrorReporter reporter,
     custom_lint.CustomLintContext context,
   ) {
-    context.registry.addComment((node) {
-      if (node.toSource().contains('TODO')) {
-        reporter.atNode(node, code);
+    context.registry.addCompilationUnit((node) {
+      final imports = node.directives.whereType<ImportDirective>().toList();
+
+      for (var i = 1; i < imports.length; i++) {
+        final prev = imports[i - 1];
+        final curr = imports[i];
+        
+        final prevUri = prev.uri.stringValue ?? '';
+        final currUri = curr.uri.stringValue ?? '';
+
+        if (prevUri.compareTo(currUri) > 0) {
+          reporter.atNode(curr, _code);
+        }
       }
     });
   }
